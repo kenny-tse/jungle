8d82077c0d4fb06a53a13c795a032652322a44e3
kenny-tse <kennytse91@gmail.com>
2021-11-12 08:50:47 +0000

    add category functions

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
+    # resources :categories, only: [:index, :new, :create]
     resources :products, except: [:edit, :update, :show]
+  end
+
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
